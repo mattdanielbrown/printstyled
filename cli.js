e0781f72e09305dd810ee618a1bc41770f58cab5
Matt Daniel Brown <developer@mattdanielbrown.com>
2020-04-06 01:34:25 -0500

    Patch to correct invalid help-message file path in cli file

--- cli.js
+++ cli.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-const meow = require('meow');
-const helpMessage = require('./util/help-message-content');
-const colortext = require('.');
-const cli = meow(helpMessage());
+const path = require('path')
+const meow = require('meow')
+const flexiblePathToHelpMessageFile = path.join(__dirname, '/util/help-message-content')
+const helpMessage = require(flexiblePathToHelpMessageFile)
+const colortext = require('.')
+const cli = meow(helpMessage())
 const otherArgs = function (cliAllInputs) {
-	if (cliAllInputs.length > 2) {
-		return cli.input.slice(2);
-	}
+  if (cliAllInputs.length > 2) {
+    return cli.input.slice(2)
+  }
 
-	return 'none';
-};
+  return 'none'
+}
 
-console.log(colortext(cli.input[0], cli.input[1], otherArgs(cli.input)));
+console.log(colortext(cli.input[0], cli.input[1], otherArgs(cli.input)))
