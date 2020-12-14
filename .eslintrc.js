82fd309262904b8d2b8f3ce572d5938b1880f203
Matt Daniel Brown <developer@mattdanielbrown.com>
2020-12-13 19:28:52 -0600

    Fixed a remaining ESLint issues.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,6 @@ module.exports = {
     ecmaVersion: 2018
   },
   rules: {
-	  "prefer-const": 1
+    'prefer-const': 1
   }
-};
+}
--- test.js
+++ test.js
@@ -1,22 +1,22 @@
-import colortext from '.';
-const test = require('ava');
+import colortext from '.'
+const test = require('ava')
 
 test('foo', t => {
-	console.log(colortext('Blue, Bold Text', 'blue', ['bold']));
-	t.pass();
-});
+  console.log(colortext('Blue, Bold Text', 'blue', ['bold']))
+  t.pass()
+})
 
 test('colorized', t => {
-	console.log(colortext('Green Text', 'green'));
-	t.pass();
-});
+  console.log(colortext('Green Text', 'green'))
+  t.pass()
+})
 
 test('stylized', t => {
-	console.log(colortext('Green, Bold, Italic Text', 'green', ['bold', 'italic']));
-	t.pass();
-});
+  console.log(colortext('Green, Bold, Italic Text', 'green', ['bold', 'italic']))
+  t.pass()
+})
 
 test('bar', async t => {
-	const bar = Promise.resolve('bar');
-	t.is(await bar, 'bar');
-});
+  const bar = Promise.resolve('bar')
+  t.is(await bar, 'bar')
+})
